54935ae1331039eabb3a62156e4617e9a6765463
Sergey Matiushkin <74297848+Theselfsame@users.noreply.github.com>
2021-07-31 17:12:15 +0300

    feat: add solution code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 != 0) {
+    return false;
+  }
+    let arr = [];
+  for (let i = 0; i < str.length; i++) {
+     for (let k = 0; k < bracketsConfig.length; k++) {
+       if ((str[i] == bracketsConfig[k][1]) && (arr[arr.length - 1] == bracketsConfig[k][0])) {
+        arr.pop();
+       } else if (str[i] == bracketsConfig[k][0]) {
+        arr.push(str[i]);
+       }
+     }
+  }
+  return (arr.length == 0);
 }
